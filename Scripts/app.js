e70fa57e48e2e5797758cf7cc9c05deadabb2ec0
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-01-16 20:34:52 -0500

    demo for insertBefore

--- Scripts/app.js
+++ Scripts/app.js
@@ -40,8 +40,30 @@
         });
 
         h2.addEventListener("mouseover", MouseOver);
-
         h2.addEventListener("mouseout", MouseOut);
+
+        // creating a new paragraph element
+        let firstParagraph = document.createElement("p");
+        // configuring the new element
+        firstParagraph.textContent = "This is a very short paragraph!";
+
+        // get a reference to the parentNode of the h2 element
+        let parentDiv = h2.parentNode;
+        
+        // insert the new paragraph before the h2 element
+        parentDiv.insertBefore(firstParagraph, h2);
+
+        // get a reference to the first h1 tag
+        let h1 = document.getElementsByTagName("h1")[0];
+
+        // copy by value from one element to another
+        let anotherParagraph = firstParagraph.cloneNode(true);
+
+        // move the paragraph above the  h1
+        h1.parentNode.insertBefore(anotherParagraph, h1);
+
+        let ul = document.getElementsByTagName("ul")[0];
+
     }
 
     window.addEventListener("load", Start);
